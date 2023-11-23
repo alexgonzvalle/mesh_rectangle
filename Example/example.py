e9ca57d611bd65716bf432db663be65753fd803d
alexgonzvalle <alexgonzvalle@gmail.com>
2023-11-23 09:31:24 +0100

    Change Example

--- Example/example.py
+++ Example/example.py
@@ -1,14 +1,14 @@
 from MeshRectangle import MeshRectangle
 
-file_bati_path = 'Garachico/batimetria.dat'
+file_bati_path = r'D:\Development\repos\Garachico\Operacional_Garachico\RESOURCES\SWAN\Batimetria_NUEVA_V2.dat'
 file_conf_path = 'Garachico/mesh.ini'
 
 # mesh_main = MeshRectangle('Main', file_mesh_ini=file_conf_path)
 # mesh_main.exec(file_bati_path)
 # mesh_main.save('Garachico/Bathymetry_mesh_main.dat')
-# mesh_main.plot_3d()
+# mesh_main.plot()
 
 mesh_nested = MeshRectangle('Nested', file_mesh_ini=file_conf_path)
 mesh_nested.exec(file_bati_path)
-mesh_nested.save('Garachico/Bathymetry_mesh_nested.dat')
-mesh_nested.plot_3d()
+# mesh_nested.save('Garachico/Bathymetry_mesh_nested.dat')
+mesh_nested.plot()
